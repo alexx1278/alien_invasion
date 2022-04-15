3893c8862ed66f1173660cb3469f109ce1612158
alexx1278 <91423688+alexx1278@users.noreply.github.com>
2022-04-15 21:07:49 +0300

    Update game_stats.py
    Добавлена функция лучший счет.

--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,5 @@
+import datetime
+
 class GameStats():
     """Отслеживание статистики для игры Alien Invasion."""
     def __init__(self, ai_settings):
@@ -7,10 +9,23 @@ class GameStats():
         # Игра запускается в неактивном состоянии.
         self.game_active = False
         # Рекорд не должен сбрасываться.
-        self.high_score = 0
+        f = open('bestscore')
+        self.high_score = int(f.read())
+        f.close()
 
     def reset_stats(self):
         """Инициализирует статистику, изменяющуюся в ходе игры."""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
         self.level = 1
+
+    def bestscore(self):
+        if self.score >= self.high_score:
+            f= open('bestscore', 'w')
+            f.write(str(self.high_score))
+            f.close()
+            f = open('stat', 'w')
+            t = {}
+            t[str(datetime.datetime.now())] = self.high_score
+            f.write(str(t))
+            f.close()
